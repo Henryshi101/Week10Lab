d94f1485f2491206d87f69bd625dca8e2c529d66
Henryshi11 <113211324+Henryshi101@users.noreply.github.com>
2022-11-12 22:37:26 -0700

    this add AdminFiler to the Week10 lab

---
+++ src/java/filters/AdminFiler.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filters;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import dataaccess.UserDB;
+
+
+/**
+ *
+ * @author 14686
+ */
+@WebFilter(filterName = "AdminFiler", servletNames = {"AdminServlet"})
+public class AdminFiler implements Filter {
+    
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+
+            // code that is executed before the servlet
+            HttpServletRequest httpRequest = (HttpServletRequest)request;
+            HttpSession session = httpRequest.getSession();
+            String email = (String)session.getAttribute("email");
+            
+            UserDB newUser = new UserDB();
+            int newUserRoleID = newUser.get(email).getRole().getRoleId();
+            if (newUserRoleID!=1) {
+                HttpServletResponse httpResponse = (HttpServletResponse)response;
+                httpResponse.sendRedirect("login");
+                return;
+            }
+            
+            chain.doFilter(request, response); // execute the servlet
+            
+            // code that is executed after the servlet
+            
+        
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        
+    }
+
+    @Override
+    public void destroy() {
+       
+    }
+}
